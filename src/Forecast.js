6283a45dfdaacd36b8b5a5134bcad085cf48b7fd
plaerke <plaerke@me.com>
2022-08-31 13:32:27 +0200

    Update Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,19 +1,20 @@
 import React from "react";
 
-export default function Forecast(props){
-return(
-  <div className="col forecast-boxes">
-    <div className="next-days">
-      <h4>{props.day}</h4>
-      <img
-        src="http://openweathermap.org/img/wn/10d@2x.png"
-        alt=""
-        width="70"
-      />
-      <div>
-      <strong className="high-temp"> 19°</strong>
-      <span className="low-temp"> 13°</span>
+export default function Forecast(props) {
+  return (
+    <div className="col forecast-boxes">
+      <div className="next-days">
+        <h4>{props.day}</h4>
+        <img
+          src="http://openweathermap.org/img/wn/10d@2x.png"
+          alt=""
+          width="70"
+        />
+        <div>
+          <strong className="high-temp"> 19°</strong>
+          <span className="low-temp"> 13°</span>
+        </div>
       </div>
     </div>
-  </div>)
+  );
 }
